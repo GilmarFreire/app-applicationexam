dd27cd6347a00d173c640c77b9cb968b6229d28d
GilmarFreire <93558838+GilmarFreire@users.noreply.github.com>
2022-11-24 20:46:04 -0300

    Inicio do Projeto app-applicationexam

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//Title da Home
+
+export default function App(){
+    return(
+        <View style={styles.container}>
+            <Text>Epistemic</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+       
   },
 });
