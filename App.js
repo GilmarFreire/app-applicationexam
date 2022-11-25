09f0471e67a6429a79f9b7540bdd7343e215d8e4
GilmarFreire <93558838+GilmarFreire@users.noreply.github.com>
2022-11-24 23:23:20 -0300

    Instalado dependencias e css

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
-import React from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react'
+import { View, StyleSheet } from "react-native";
+import { Button, Icon, Input, Text } from 'react-native-elements';
+import styles from './styles/MainStyle';
+
 
 //Title da Home
-
 export default function App(){
+
+const [email, setEmail] = useState(null)
+const [password, setPassword] = useState(null)
+
+const entrar = () => {
+  console.log('entrou')
+  console.log(email)
+  console.log(password)
+}
+
     return(
         <View style={styles.container}>
-            <Text>Epistemic</Text>
-        </View>
+            <Text h3>Epistemic</Text>
+                <Input
+                    placeholder="Entre com seu E-mail"
+                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                    onChangeText={value => setEmail(value)}
+                    keyboardType='email-address'
+                />
+
+                <Input
+                    placeholder="Digite sua senha para entrar"
+                    leftIcon={{ type: 'font-awesome', name: 'key' }}
+                    onChangeText={value => setPassword(value)}
+                    secureTextEntry={true}
+                />
+
+                <Button
+                    icon={
+                      <Icon name="home" color="white" />
+                    }
+                    title='ENTRAR'                  
+                    onPress={() => entrar()}
+                />
+          </View>
     );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-       
-  },
-});
