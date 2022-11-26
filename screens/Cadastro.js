56f3fee1edcc7ef0b1ff257d7895560712168061
GilmarFreire <93558838+GilmarFreire@users.noreply.github.com>
2022-11-25 23:17:22 -0300

    Definição de Regex para formulários
    Regex
    Email

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -10,15 +10,33 @@ export default function Cadastro({navigation}){
 
 const [email, setEmail] = useState(null)
 const [password, setPassword] = useState(null)
+const [errorEmail, setErrorEmail] = useState(null)
 
+const validar = () => {
+    let error = false
+    
+    setErrorEmail(null)
 
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
 
-const entrar = () => {
-  navigation.reset({
-    index: 0, 
-    routes: [{name: "Home"}]
-  })
+    if(!re.test(email)) {
+        alert("Houve um erro ao cadastrar o usuario, tente novamente")
+        setErrorEmail("Preencha seu e-mail corretamente")
+        error = true
+    } 
+    
+    return !error   
 }
+
+const salvar = () => {
+    if(validar()){
+        alert("Usuario Cadastrado com Sucesso")
+        navigation.reset({
+            index: 0,
+            routes: [{name: "Home"}]})
+    }                
+} 
 
 const cadastrar = () => {
     navigation.navigate("Cadastro")
@@ -27,11 +45,15 @@ const cadastrar = () => {
     return(
         <View style={styles.container}>
             <Text h3>Cadastre-se</Text>
-                <Input
-                    placeholder="Entre com seu E-mail"
-                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                    onChangeText={value => setEmail(value)}
-                    keyboardType='email-address'
+            <Input 
+                    placeholder="Digite um email valido"
+                    leftIcon={{type: 'font-awesome', name: 'envelope'}}
+                    keyboardType="email-address"
+                    onChangeText={value => {
+                        setEmail(value)
+                        setErrorEmail(null)
+                    }}
+                    errorMessage={errorEmail}
                 />
                 
                 <Button
@@ -44,7 +66,7 @@ const cadastrar = () => {
                     }
                     title="Entrar"
                     buttonStyle={styles.button}
-                    onPress={() => entrar()}
+                    onPress={() => salvar()}
                 />
           </View>
     );
--- screens/Login.js
+++ screens/Login.js
@@ -10,35 +10,65 @@ export default function Login({navigation}){
 
 const [email, setEmail] = useState(null)
 const [password, setPassword] = useState(null)
+const [errorEmail, setErrorEmail] = useState(null)
+const [errorPassword, setErrorPassword] = useState(null)
+
+const validar = () => {
+    let error = false
+    setErrorEmail(null)
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+
+    if(!re.test(email)) {
+        alert("Usuario não encontrado, digite o campo corretamente")
+        setErrorEmail("Preencha seu e-mail corretamente")
+        error = true
+    } 
+    if(password == null){
+        alert("Senha incorreta ou espaço vazio, tente novamente")
+        setErrorPassword("Preencha sua senha corretamente")
+        error = true
+    }
+    return !error   
+}
 
 const entrar = () => {
-  navigation.reset({
-    index: 0, 
-    routes: [{name: "Home"}]
-  })
+    if(validar()){
+        alert("Seja Bem Vindo!!!")
+        navigation.reset({
+            index: 0,
+            routes: [{name: "Home"}]})
+    }
 }
 
 const cadastrar = () => {
     navigation.navigate("Cadastro")
 }
 
-    return(
-
-        
+    return(        
         <View style={styles.container}>
             <Text h3>Epistemic</Text>
-                <Input
-                    placeholder="Entre com seu E-mail"
-                    leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                    onChangeText={value => setEmail(value)}
-                    keyboardType='email-address'
+            <Input 
+                    placeholder="Digite um email valido"
+                    leftIcon={{type: 'font-awesome', name: 'envelope'}}
+                    keyboardType="email-address"
+                    onChangeText={value => {
+                        setEmail(value)
+                        setErrorEmail(null)
+                    }}
+                    errorMessage={errorEmail}
                 />
-
-                <Input
-                    placeholder="Digite sua senha para entrar"
-                    leftIcon={{ type: 'font-awesome', name: 'key' }}
-                    onChangeText={value => setPassword(value)}
-                    secureTextEntry={true}
+                
+               <Input 
+                    placeholder="Digite seu password"
+                    leftIcon={{type: 'font-awesome', name: 'key'}}
+                    onChangeText={value => {
+                        setPassword(value)
+                        setErrorPassword(null)   
+                    }}   
+                    secureTextEntry={true}  
+                    errorMessage={errorPassword}             
                 />
 
                 <Button
